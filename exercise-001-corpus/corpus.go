b8644840057337a8ead31fcab591167f510a265c
Andrew Chen <chenan02@gmail.com>
2016-07-03 11:14:30 -0500

    Get Exercise 1 passing sufficiently

--- exercise-001-corpus/corpus.go
+++ exercise-001-corpus/corpus.go
@@ -17,7 +17,10 @@ func main() {
     log.Fatal(err)
   }
   defer file.Close()
-  
+  // abstract to other libraries, raise error in main & return 
+  // return err to here, raise here
+  // keep as much code as possible outta here
+ 
   // Read counts of words into map
   wordMap := make(map[string]int)
   scanner := bufio.NewScanner(file)
@@ -31,6 +34,7 @@ func main() {
 
   wordCounts := wordcount.BuildWordCountSlice(wordMap)
   sort.Sort(wordcount.ByCount(wordCounts))
+  // custom wordCount printLn method in WordCount library, can make Word and Count "private"
   for _, x := range wordCounts {
     fmt.Println(x.Word, x.Count)
   }
--- exercise-001-corpus/wordcount/wordcount_test.go
+++ exercise-001-corpus/wordcount/wordcount_test.go
@@ -3,39 +3,55 @@ package wordcount
 import (
   "testing"
   "sort"
+  "github.com/stretchr/testify/assert"
 )
 
-type testpair struct {
-  map map[string]int
-  sorted []wordCount
-}
-
-standardMap := testpair{
-  {"hello":3,
-  "hey":3,
-  "yo":1,
-  "dood":2},
-  []wordCount{}
-}
-
-equalMap := map[string]int{
-  "yo":2,
-  "ok":2,
-  "hey":2
-}
-
-oneWordMap := map[string]int{
-  "whatsup":5
-}
-
-emptyMap := map[string]int{
-}
-
-
 func TestStandard(t *testing.T) {
-  wordCounts := OrderWords(standardMap)
-  sort.Sort(ByCount(wordCounts))
-  if wordCounts != 
+  expectedSort := []WordCount{{"hello",3}, {"hey",3}, {"dood",2}, {"yo",1}}
+  standardMap := map[string]int{"hey":3, "yo":1, "dood":2, "hello":3}
+  wordCounts := BuildWordCountSlice(standardMap)
+  if assert.NotNil(t, wordCounts) {
+    assert.Equal(t, len(wordCounts), 4, "The WordCount slice should have correct size")
+    sort.Sort(ByCount(wordCounts))
+    assert.Equal(t, wordCounts, expectedSort, "The sort functions should sort correctly")
+  }
 }
 
- 
+func TestEmpty(t *testing.T) {
+  emptyMap := map[string]int{}
+  wordCounts := BuildWordCountSlice(emptyMap)
+  assert.Nil(t, wordCounts, "The wordCount slice should be nil")
+}
+
+func TestSameCounts(t *testing.T) {
+  expectedSort := []WordCount{{"cool",4}, {"sup",4}, {"yo",4}}
+  sameMap := map[string]int{"yo":4, "cool":4, "sup":4}
+  wordCounts := BuildWordCountSlice(sameMap)
+  if assert.NotNil(t, wordCounts) {
+    assert.Equal(t, len(wordCounts), 3, "The WordCount slice should have correct size")
+    sort.Sort(ByCount(wordCounts))
+    assert.Equal(t, wordCounts, expectedSort, "The sort function should break tiebreakers alphabetically")
+  }
+}
+
+func TestNonAlphaWords(t *testing.T) {
+  expectedSort := []WordCount{{"1",4}, {"3",4}, {"2",1}, {"4",1}}
+  nonAlphaMap := map[string]int{"1":4, "2":1, "3":4, "4":1}  
+  wordCounts := BuildWordCountSlice(nonAlphaMap)
+  if assert.NotNil(t, wordCounts) {
+    assert.Equal(t, len(wordCounts), 4, "The WordCount slice should have correct size")
+    sort.Sort(ByCount(wordCounts))
+    assert.Equal(t, wordCounts, expectedSort, "The sort function should sort number keys lowest first to highest")
+  }
+} 
+
+func TestAlphaNumericWords(t *testing.T) {
+  expectedSort := []WordCount{{"2",2}, {"b",2}, {"1",1}, {"a",1}}
+  alphaNumericMap := map[string]int{"a":1, "b":2, "1":1, "2":2}
+  wordCounts := BuildWordCountSlice(alphaNumericMap)
+  if assert.NotNil(t, wordCounts) {
+    assert.Equal(t, len(wordCounts), 4, "The WordCount slice should have correct size")
+    sort.Sort(ByCount(wordCounts))
+    assert.Equal(t, wordCounts, expectedSort, "The sort function should assign higher priority to numbers")
+  }
+}
