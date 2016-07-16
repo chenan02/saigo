bce166a18173fa351a1d069a117280fcffb24a95
Andrew Chen <chenan02@gmail.com>
2016-07-16 18:29:10 -0500

    File storage for persistance

--- exercise-003-web/server.go
+++ exercise-003-web/server.go
@@ -3,10 +3,40 @@ package main
 import (
   "html/template"
   "net/http"
+  "os"
+  "bufio"
 )
 
+func writeToFile(name string) {
+  f, err := os.OpenFile("names.txt", os.O_APPEND|os.O_WRONLY, 0600)
+  if err != nil {
+    panic(err)
+  }
+  defer f.Close()
+  if _, err := f.WriteString(name + "\n"); err != nil {
+    panic(err)
+  }
+}
+
+func readFromFile() map[string]int {
+  nc := make(map[string]int)
+  f, err := os.Open("names.txt")
+  if err != nil {
+    panic(err)
+  }
+  defer f.Close()
+  scanner := bufio.NewScanner(f)
+  scanner.Split(bufio.ScanWords)
+  for scanner.Scan() {
+    nc[scanner.Text()]++
+  }
+  if err := scanner.Err(); err != nil {
+    panic(err)
+  }
+  return nc
+}
+
 var indexT = template.Must(template.ParseFiles("index.html"))
-var nc = make(map[string]int)
 
 func index(w http.ResponseWriter, r *http.Request) {
   indexT.Execute(w, nil)
@@ -15,7 +45,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 func addName(w http.ResponseWriter, r *http.Request) {
   r.ParseForm()
   name := r.Form.Get("name")
-  nc[name]++  
+  writeToFile(name)  
+  nc := readFromFile()
   indexT.Execute(w, &nc)
 }
 
